b882f530e816f018db9ba17e239611a37584b613
cultstatue <saltedchurros@gmail.com>
2022-08-02 18:30:47 -0400

    added both get and post routes

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+// require neccessary modules
 const express = require('express');
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require('./data/notes.json')
 
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
