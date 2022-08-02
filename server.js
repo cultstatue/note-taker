dfbe4cb5615ed3d5a260a64bf43e369616c403f2
cultstatue <saltedchurros@gmail.com>
2022-08-02 18:08:38 -0400

    restructured some files and established server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const { notes } = require('./data/notes.json')
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
