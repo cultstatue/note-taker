fae3ffadf1f02f743763bc80d6462f6838b2fc18
cultstatue <saltedchurros@gmail.com>
2022-08-02 19:14:46 -0400

    added routes to serve api and  html requests

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // require neccessary modules
 const express = require('express');
+const fs = require('fs');
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const uniqid = require('uniqid'); 
 const { notes } = require('./data/notes.json')
 
 // middleware
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -13,9 +16,38 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+function createNewNote(body, notesArray) {
+
+    const note = body;
+    notesArray.push(note);
+
+    fs.writeFileSync(
+
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    )
+  
+    return note;
+}
 app.post('/api/notes', (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+
+    req.body.id = uniqid();
+
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
