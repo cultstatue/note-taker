8d85a6ff8534ee9215715a49aeb29984fc563675
cultstatue <saltedchurros@gmail.com>
2022-08-02 20:14:36 -0400

    added heroku deployment

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const { notes } = require('./data/notes.json')
-
 // middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
